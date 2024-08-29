9cfafa720738cb09e1a7872ddd7096fc2a0bb807
PhamBaBac <bacphamba082@gmail.com>
2024-08-29 11:10:39 +0700

    fix: navigator

--- App.tsx
+++ App.tsx
@@ -4,23 +4,9 @@ import { fontFamilies } from './src/constants';
 
 const App = () => {
   return (
-    <View>
-      <Text style={{fontSize: 18, color: 'red', fontFamily: fontFamilies.bold}}>
-        Hello word!
-      </Text>
-      <Text
-        style={{fontSize: 18, color: 'red', fontFamily: fontFamilies.semiBold}}>
-        Hello word!
-      </Text>
-      <Text
-        style={{fontSize: 18, color: 'red', fontFamily: fontFamilies.medium}}>
-        Hello word!
-      </Text>
-      <Text
-        style={{fontSize: 18, color: 'red', fontFamily: fontFamilies.regular}}>
-        Hello word!
-      </Text>
-    </View>
+    <NavigationContainer>
+      <AppRouters />
+    </NavigationContainer>
   );
 }
 
