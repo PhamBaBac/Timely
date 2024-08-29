c1f3a949883cf9cff252b7b892aa63d73348a8ce
PhamBaBac <bacphamba082@gmail.com>
2024-08-29 11:11:11 +0700

    fix: navigator

---
+++ src/screens/index.ts
@@ -0,0 +1,24 @@
+import AddNewScreen from "./AddNewScreen";
+import ForgotPassword from "./auth/ForgotPassword";
+import LoginScreen from "./auth/LoginScreen";
+import OnbroadingScreen from "./auth/OnbroadingScreen";
+import SignUpScreen from "./auth/SignUpScreen";
+import Verification from "./auth/Verification";
+import CalendarScreen from "./calendar/CalendarScreen";
+import HomeScreen from "./home/HomeScreen";
+import ProfileScreen from "./profiles/ProfileScreen";
+import Teamwork from "./teamwork/teamworkScreen";
+
+
+export {
+  ForgotPassword,
+  LoginScreen,
+  OnbroadingScreen,
+  SignUpScreen,
+  Verification,
+  AddNewScreen,
+  CalendarScreen,
+  HomeScreen,
+  ProfileScreen,
+  Teamwork,
+};
