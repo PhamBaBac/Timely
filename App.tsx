7e197c4f1c5d23dbf53221bad085b36f87407c0e
PhamBaBac <bacphamba082@gmail.com>
2024-08-29 11:11:11 +0700

    fix: navigator

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { fontFamilies } from './src/constants';
-
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import AppRouters from './src/routers/AppRouters';
 const App = () => {
   return (
     <NavigationContainer>
