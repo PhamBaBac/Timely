ddf389fb11e1d87178f33d0b6cb792f57d910c8b
PhamBaBac <bacphamba082@gmail.com>
2024-10-25 18:08:03 +0700

    fix: add taskscreen

--- src/routers/ProfileNavigator.tsx
+++ src/routers/ProfileNavigator.tsx
@@ -1,7 +1,6 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
 import {ProfileScreen} from '../screens';
-import TaskListScreen from '../screens/profiles/TaskListScreen';
 
 const ProfileNavigator = () => {
   const Stack = createNativeStackNavigator();
@@ -11,7 +10,6 @@ const ProfileNavigator = () => {
         headerShown: false,
       }}>
       <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
-      <Stack.Screen name="TaskListScreen" component={TaskListScreen} />
     </Stack.Navigator>
   );
 };
---
+++ src/utils/handleNotification.ts
@@ -0,0 +1,145 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
+import messaging from '@react-native-firebase/messaging';
+const serviceAccount = require('../../service-account.json');
+
+const user = auth().currentUser;
+
+export class HandleNotification {
+  static checkNotificationPersion = async () => {
+    const authStatus = await messaging().requestPermission();
+
+    if (
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL
+    ) {
+      this.getFcmToken();
+    }
+  };
+
+  static getFcmToken = async () => {
+    const fcmtoken = await AsyncStorage.getItem('fcmtoken');
+    console.log('fcmtoken:', fcmtoken);
+    if (!fcmtoken) {
+      const token = await messaging().getToken();
+      if (token) {
+        await AsyncStorage.setItem('fcmtoken', token);
+        this.UpdateToken(token);
+      }
+    }
+  };
+
+  static UpdateToken = async (token: string) => {
+    const currentUser = auth().currentUser;
+    if (currentUser) {
+      await firestore()
+        .doc(`users/${currentUser.uid}`)
+        .get()
+        .then(snap => {
+          if (snap.exists) {
+            const data: any = snap.data();
+
+            if (!data.tokens || !data.tokens.includes(token)) {
+              firestore()
+                .doc(`users/${currentUser.uid}`)
+                .update({
+                  tokens: firestore.FieldValue.arrayUnion(token),
+                });
+            }
+          }
+        });
+    }
+  };
+
+  static getAccessToken = async () => {
+    try {
+      const res = await fetch(
+        'https://server-todolist-5jwg.onrender.com/get-accesstoken',
+        {
+          method: 'post',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            email: serviceAccount.client_email,
+            key: serviceAccount.private_key,
+          }),
+        },
+      );
+      const result = await res.json();
+      const accessToken = result.data.access_token;
+      return accessToken;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  static SendNotification = async ({
+    memberId,
+    title,
+    body,
+    taskId,
+  }: {
+    memberId: string;
+    title: string;
+    body: string;
+    taskId: string;
+  }) => {
+    try {
+      // Save to Firestore
+      await firestore().collection('notifications').add({
+        isRead: false,
+        createdAt: Date.now(),
+        updatedAT: Date.now(),
+        title,
+        body,
+        taskId,
+        uid: memberId,
+      });
+
+      // Send Notification
+      const member: any = await firestore().doc(`users/${memberId}`).get();
+      console.log('member:', member);
+
+      if (member && member.data().tokens) {
+        var myHeaders = new Headers();
+        myHeaders.append('Content-Type', 'application/json');
+        myHeaders.append(
+          'Authorization',
+          `Bearer ${await this.getAccessToken()}`,
+        );
+
+        var raw = JSON.stringify({
+          message: {
+            token: member.data().tokens[member.data().tokens.length - 1],
+            notification: {
+              title,
+              body,
+            },
+            data: {
+              taskId,
+            },
+          },
+        });
+
+        var requestOptions: any = {
+          method: 'POST',
+          headers: myHeaders,
+          body: raw,
+          redirect: 'follow',
+        };
+
+        fetch(
+          'https://fcm.googleapis.com/v1/projects/todolistapp-clone/messages:send',
+          requestOptions,
+        )
+          .then(response => response.json())
+          .then(result => console.log(result))
+          .catch(error => console.log('error', error));
+      }
+    } catch (error) {
+      console.log('Error sending notification:', error);
+    }
+  };
+}
